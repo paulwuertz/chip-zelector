27d9506c22bbd18dd704bf9f2d2b4c5fedb1149c
paulwuertz <s3336462@msx.tu-dresden.de>
2021-12-12 01:59:06 +0100

    build all chips hello world to copy their .dts files ^^

---
+++ build_all_dts.sh
@@ -0,0 +1,7 @@
+mkdir -p dts_temp
+
+while IFS= read -r line; do
+  echo west build -p -b $line ../../samples/hello_world
+  west build -p -b $line ../../samples/hello_world
+  cp build/zephyr/zephyr.dts dts_temp/$line.dts
+done < device_list.txt
