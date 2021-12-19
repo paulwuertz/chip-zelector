867e3dc71f0d45cd7be8be16a66557c1f8e61835
paulwuertz <s3336462@msx.tu-dresden.de>
2021-12-19 14:11:32 +0100

    extract arch of board from docs

--- socmap.py
+++ socmap.py
@@ -9,12 +9,27 @@ interesting_props = [
     "usart", "adc", "dac", "pwm", "usb", "ethernet", "sdmmc", "cpus",
     "flash", "can", "timers", "serial", "wdg", "quadspi"
 ]
+json_board_props = []
 board_interesting_feature_map = {}
+boards = [f for f in os.listdir(dts_path) if f.endswith(".dts")]
+arches = ["arc", "arm", "arm64", "nios2", "posix", "riscv", "sparc", "x86", "xtensa"]
 
+def get_arch_and_docu_map():
+    """traverse the boards dir and generate a board to arch+docu map"""
+    devices = {d:{"arch": None, "docu_html": ""} for d in open("device_list.txt", "r").read().split("\n")}
+    for arch in arches:
+        # get the arch
+        for board in os.listdir("../../boards/"+arch):
+            if board not in devices: continue
+            devices[board]["arch"] = arch
+            print(arch, board, board in devices)
+    return devices
+
+#get_arch_and_docu_map()
+#exit()
 #
 # Extract some features as dt tree
 #
-boards = [f for f in os.listdir(dts_path) if f.endswith(".dts")]
 for board in boards:
     board_name = board.replace(".dts", "")
     props = {p:[] for p in interesting_props}
@@ -28,7 +43,6 @@ for board in boards:
 #
 # Features dt to simpler json
 #
-json_board_props = []
 for board_name, dt_preselect_props in board_interesting_feature_map.items():
     json_board_prop = {
         "cpus": {"cores_count": len(dt.get_node("/cpus").nodes)}
@@ -67,8 +81,9 @@ for board_name, dt_preselect_props in board_interesting_feature_map.items():
                 "label": f.props["label"].to_string() if "label" in f.props else "NO_LABEL"
             } for f in dt_preselect_props[periph]]
         }
-
+    arch_doc_map = get_arch_and_docu_map()
     json_board_prop["name"] = board_name
+    json_board_prop["arch"] = arch_doc_map[board_name]["arch"]
     json_board_props += [json_board_prop]
 
 #
